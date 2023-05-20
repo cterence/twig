d2643bb8ad9a1e8dddc1498b962fc953b5b22c02
Térence Chateigné <terence.chateigne@posteo.net>
2023-05-20 02:29:58 +0200

    remove burrito test

--- burrito-test/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket                      = "twig-tfstates"
-    key                         = "burrito-test"
-    endpoint                    = "https://d2c3d37341116095bc4b4f533dc5c487.r2.cloudflarestorage.com"
-    region                      = "us-east-1"
-    skip_credentials_validation = true
-  }
-}
